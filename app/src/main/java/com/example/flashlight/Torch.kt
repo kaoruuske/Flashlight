623b6789e9e797b0207cf89d463259fccf6e9dae
kaoruuske <kaoruuske@naver.com>
2019-08-20 19:54:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/flashlight/Torch.kt
@@ -0,0 +1,44 @@
+package com.example.flashlight
+
+import android.content.Context
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+
+class Torch(context: Context) {
+    private var cameraId: String? = null
+    private val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+    init {
+        cameraId = getCameraId()
+    }
+
+
+    //cameraId null 값이 상속되면 안되는 경우? 이기 때문에 !! 추가
+    fun flashOn() {
+        cameraManager.setTorchMode(cameraId!!, true)
+    }
+
+    fun flashOff() {
+        cameraManager.setTorchMode(cameraId!!, false)
+    }
+
+    private fun getCameraId(): String? {
+        val cameraIds = cameraManager.cameraIdList
+        for (id in cameraIds) {
+            val info = cameraManager.getCameraCharacteristics(id)
+            val flashAvailable = info.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)
+            val lensFacing = info.get(CameraCharacteristics.LENS_FACING)
+            if (flashAvailable != null
+                && flashAvailable
+                && lensFacing != null
+                && lensFacing == CameraCharacteristics.LENS_FACING_BACK
+            ) {
+                return id
+            }
+        }
+
+        return null
+
+    }// end getCameraId()
+
+}
